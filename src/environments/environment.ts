3eaa3a1bac8921a0be63322adf726041a09adef2
maheshyaddanapudi <zzzmahesh@gmail.com>
2021-04-08 00:48:07 +0530

    Updating to Angular 11

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
    OAUTH_TOKEN_URL: 'http://localhost:9990/auth/realms/master/protocol/openid-connect/token',
    OAUTH_USER_INFO_URL: 'http://localhost:8080/userinfo',
    OAUTH_LOGOUT_URL: 'http://localhost:9990/auth/realms/master/protocol/openid-connect/logout',
-   OAUTH_ENABLED: 'N',
+   OAUTH_ENABLED: 'Y',
    OAUTH_CLIENT_ID: 'conductor_user_client',
    OAUTH_CLIENT_SECRET: '8782ca99-decc-441a-8988-736350fafe67',
    LOG_AGGREGATOR_ANALYTICS_IFRAME_URL: 'undefined',
