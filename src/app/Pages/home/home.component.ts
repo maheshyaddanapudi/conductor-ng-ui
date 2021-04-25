48281524d5e19a6513e715c0ea54411b9890a31d
Mahesh Yaddanapudi <zzzmahesh@gmail.com>
2021-04-25 21:41:41 +0530

    Removed unnecessary imports

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { CronOptions } from 'cron-editor';
 import { environment } from '../../../environments/environment'
 
 declare const SwaggerUIBundle: any;
@@ -11,30 +10,6 @@ declare const SwaggerUIBundle: any;
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  public cronExpression = '0 0/5 * 1/1 * ? *';
-  public isCronDisabled = false;
-  public cronOptions: CronOptions = {
-    formInputClass: 'cron-editor-material-control',
-    formSelectClass: 'cron-editor-material-control',
-    formRadioClass: 'cron-editor-radio',
-    formCheckboxClass: 'cron-editor-checkbox',
-
-    defaultTime: '10:00:00',
-    use24HourTime: true,
-
-    hideMinutesTab: false,
-    hideHourlyTab: false,
-    hideDailyTab: false,
-    hideWeeklyTab: false,
-    hideMonthlyTab: false,
-    hideYearlyTab: false,
-    hideAdvancedTab: false,
-
-    hideSeconds: true,
-    removeSeconds: false,
-    removeYears: false
-  };
 
   public server_swagger_url: string;
 
